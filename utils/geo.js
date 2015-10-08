a90ea880b3bd43f1a304ac6e7186369f6c6559d4
spygi <spygiannakis@hotmail.com>
2015-10-08 19:41:34 +0200

    the places api changed in the meantime?

--- utils/geo.js
+++ utils/geo.js
@@ -5,12 +5,13 @@ module.exports = {
 	getNearbyStores: function(location, callback){
 		googlePlaces.placeSearch({
 			location: [location.latitude, location.longitude],
-					radius: 300, // probably meters
+					radius: 5000, // probably meters
 					types: "grocery_or_supermarket",
 					// opennow: true // for testing in the night :)
-	}, function(response){
-		if(response && response.length > 0){
-			callback(response[0].results)
+	}, function(error, response){
+		
+		if(response && response.results){
+			callback(response.results)
 		}else{
 			callback([]);
 		}
